8569a93a14a6a420db14efb1dfce84ae410d020d
Usama <usamahamid324@gmail.com>
2023-07-23 21:17:44 +0500

    AdmissionForm

---
+++ demo-app/src/App.js
@@ -0,0 +1,20 @@
+
+// import SignIn from './components/SignIn';
+// import { Counter } from './components/Counter';
+
+// import AdmissionForm from "./components/AdmissionForm";
+import FinalComponent from "./AdmissionForm/FinalComponent"
+
+function App() {
+  
+  return (
+   <>
+   {/* <SignIn/> */}
+   {/* <Counter/> */}
+   <FinalComponent/>
+   </>
+  );
+}
+
+export default App;
+
